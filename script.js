781c3d282e90c62e66ed114de5e3d04963e93af1
FelSaya-ao <felicitydomingo12@gmail.com>
2024-09-13 14:32:57 +0800

    Added stuff

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+const backToTopButton = document.getElementById('back-to-top');
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+};
+
+backToTopButton.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
