f3fddd0f7d8ad389d59ab5853f74bb2c7a799a74
FelSaya-ao <felicitydomingo12@gmail.com>
2024-09-13 10:04:00 +0800

    Updated html and js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert('Welcome to my website!');
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
